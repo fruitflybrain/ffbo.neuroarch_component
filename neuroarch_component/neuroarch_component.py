36e47c05a382e24603c95370d85e890497b2c83c
Yiyin Zhou <yiyin.zhou@fordham.edu>
2026-06-29 23:50:12 -0400

    reinstate the intermediate certificate

--- neuroarch_component/neuroarch_component.py
+++ neuroarch_component/neuroarch_component.py
@@ -77,7 +77,7 @@ realm = config["SERVER"]["realm"]
 authentication = eval(config["AUTH"]["authentication"])
 debug = eval(config["DEBUG"]["debug"])
 ca_cert_file = config["AUTH"]["ca_cert_file"]
-#intermediate_cert_file = config["AUTH"]["intermediate_cert_file"]
+intermediate_cert_file = config["AUTH"]["intermediate_cert_file"]
 
 # Required to handle dill's inability to serialize namedtuple class generator:
 setattr(pyorient.ogm.graph, 'orientdb_version',
@@ -1704,9 +1704,9 @@ if __name__ == '__main__':
     parser.add_argument('--ca_cert', dest='ca_cert_file', type=six.text_type,
                         default=ca_cert_file,
                         help='Root CA PEM certificate file (defaults to value from config.ini).')
-    #parser.add_argument('--int_cert', dest='intermediate_cert_file', type=six.text_type,
-    #                    default=intermediate_cert_file,
-    #                    help='Intermediate PEM certificate file (defaults to value from config.ini).')
+    parser.add_argument('--int_cert', dest='intermediate_cert_file', type=six.text_type,
+                        default=intermediate_cert_file,
+                        help='Intermediate PEM certificate file (defaults to value from config.ini).')
     parser.add_argument('--no-ssl', dest='ssl', action='store_false')
     parser.add_argument('--database', dest='db', type=six.text_type, default='na_server',
                         help='Orientdb database folder name.')
@@ -1758,23 +1758,19 @@ if __name__ == '__main__':
              'dataset': dataset, 'name': name, 'mode': args.mode}
 
     if args.ssl:
-        import re
-        from OpenSSL import crypto
+        st_cert=open(args.ca_cert_file, 'rt').read()
+        c=OpenSSL.crypto
+        ca_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
 
-        with open(args.ca_cert_file, 'rt') as f:
-            ca_data = f.read()
+        st_cert=open(args.intermediate_cert_file, 'rt').read()
+        intermediate_cert=c.load_certificate(c.FILETYPE_PEM, st_cert)
 
-        pem_blocks = re.findall(
-            r'-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----',
-            ca_data, re.DOTALL)
-        ca_certs = [crypto.load_certificate(crypto.FILETYPE_PEM, b)
-                    for b in pem_blocks]
-
-        certs = OpenSSLCertificateAuthorities(ca_certs)
+        certs = OpenSSLCertificateAuthorities([ca_cert, intermediate_cert])
         ssl_con = CertificateOptions(trustRoot=certs)
 
-        runner = ApplicationRunner(url=args.url, realm=args.realm,
-                                   extra=extra, ssl=ssl_con)
+        # now actually run a WAMP client using our session class ClientSession
+        runner = ApplicationRunner(url = args.url, realm = args.realm,
+                                   extra = extra, ssl = ssl_con)
 
     else:
         # now actually run a WAMP client using our session class ClientSession
